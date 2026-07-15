875a1f7eb325ae0be72bb6d722e1c855250ad2a9
omoladeajala <shadowcoderx.02@gmail.com>
2026-07-15 22:45:32 +0100

    feat: implement DataTable component to display records in a structured table format

--- src/component/DataTable.jsx
+++ src/component/DataTable.jsx
@@ -1,5 +1,26 @@
-function DataTable() {
-  return <div>ta</div>;
+import TableRow from "./TableRow";
+
+function DataTable({ records }) {
+  return (
+    <div>
+      <table className="w-full bg-white rounded-lg shadow overflow-hidden">
+        <thead className="bg-gray-100">
+          <tr>
+            <th className="p-4">Customer</th>
+            <th className="p-4">Amount</th>
+            <th className="p-4">Status</th>
+            <th className="p-4">Action</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {records.map((record) => (
+            <TableRow key={record.id} record={record} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default DataTable;
